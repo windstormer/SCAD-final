b8e213fa00f878285e1ccb2fb78a3bd835ed6f77
windstormer <chenjuzen@gmail.com>
2016-12-22 16:36:01 +0800

    mixed-content bug

--- extension/content_script.js
+++ extension/content_script.js
@@ -12,6 +12,15 @@ popup.style.width = "50%";
 popup.style.zIndex = "999";
 popup.style.backgroundColor = "blue";
 popup.style.visibility = "hidden";
+var keyword = document.createElement("input");
+keyword.id="input";
+var button = document.createElement("button");
+button.onclick = search;
+button.innerHTML = "Go!"
+popup.appendChild(keyword);
+popup.appendChild(button);
+
+
 
 
 
@@ -39,5 +48,58 @@ $("body").append(popup);
 input = document.getElementsByClassName("picture_talk");
 for (var i = input.length - 1; i >= 0; i--) {
 	input[i].onclick = check;
+}
+
+
+function search()
+{
+  var tag = document.getElementById("input").value;
+  console.log(tag);
+
+var jsontree = null;
+$.ajax({
+    url: "//coldegarage.tech/~demo1/curl.php",
+    //url: "http://140.114.206.88/curl.php",
+    type: "GET",
+    data:{
+      "tag":tag
+    },
+    dataType: 'JSON',
+    success: function(result){
+        jsontree = result;
+        //console.log(jsontree);
+        display(jsontree);
+    },
+    error:function(){
+        alert('fail');
+    }
+});
+
+
+}
+
+function display(jsontree)
+{
+  if(jsontree!=null)
+{
+  var json = jsontree = JSON.stringify(jsontree);
+  var obj = jQuery.parseJSON(json);
+  var images = obj["images"];
+  var keyword = obj["keyword"];
+
+  var list = document.getElementById("list");
+  
+  for(var i =0; i < images.length; i++)
+  {
+  var img = document.createElement('img');
+  img.src = images[i]["src"];
+  $(".popup").append(img);
+  }
+  
+
+  //parse json
+}else
+  alert("No result");
+
 }
 
