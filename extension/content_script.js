1606657459d357863792f98f4117a0cb1acee342
windstormer <chenjuzen@gmail.com>
2016-12-22 15:42:16 +0800

    fix appear more than one button bug

--- extension/content_script.js
+++ extension/content_script.js
@@ -1,81 +1,43 @@
 console.log("test!!!");
 
-var output = document.createElement("button");
-output.innerHTML = "test";
-output.className = "picture_talk";
+
+
+var popup = document.createElement("div");
+popup.className = "image_box";
+popup.style.top = "10%";
+popup.style.left = "25%";
+popup.style.position = "fixed";
+popup.style.height = "50%";
+popup.style.width = "50%";
+popup.style.zIndex = "999";
+popup.style.backgroundColor = "blue";
+popup.style.visibility = "hidden";
+
 
 
 function check(){
-  //alert("hello");
-  var popup = document.createElement("div");
-  popup.className = "hi";
-  popup.style.top = "0";
-  popup.style.position = "fixed";
-  popup.style.height = "50%";
-  popup.style.width = "50%";
-  popup.style.zIndex = "999";
-  popup.style.backgroundColor = "blue";
-
-  $("body").append(popup);
+  if(popup.style.visibility === 'hidden'){
+    popup.style.visibility = 'visible';
+  } else {
+    popup.style.visibility = 'hidden';
+  }
 }
 
+var list = document.getElementsByClassName("UFICommentAttachmentButtons");
 
-var buttons = $(".UFICommentAttachmentButtons");
-buttons.append(output);
+for(var i =0 ;i<list.length;i++)
+{
+  var output = document.createElement("button");
+  output.innerHTML = "test";
+  output.className = "picture_talk";
+  if(list[i].childNodes.length==2)  //prevent from append more than one time
+    list[i].appendChild(output);
+}
+
+$("body").append(popup);
 
 input = document.getElementsByClassName("picture_talk");
 for (var i = input.length - 1; i >= 0; i--) {
 	input[i].onclick = check;
 }
 
-
-function search()
-{
-  var tag = document.getElementById("input").value;
-
-var jsontree = null;
-$.ajax({
-    url: "http://coldegarage.tech/~demo1/curl.php",
-    //url: "http://140.114.206.88/curl.php",
-    type: "GET",
-    data:{
-      "tag":tag
-    },
-    dataType: 'JSON',
-    success: function(result){
-        jsontree = result;
-        //console.log(jsontree);
-        display(jsontree);
-    },
-    error:function(){
-        alert('fail');
-    }
-});
-
-
-}
-
-function display(jsontree)
-{
-  if(jsontree!=null)
-{
-  var json = jsontree = JSON.stringify(jsontree);
-  var obj = jQuery.parseJSON(json);
-  var images = obj["images"];
-  var keyword = obj["keyword"];
-
-  var list = document.getElementById("list");
-  
-  for(var i =0; i < images.length; i++)
-  {
-  var img = document.createElement('img');
-  img.src = images[i]["src"];
-  $("#list").append(img);
-  }
-  
-
-  //parse json
-}else
-  alert("No result");
-
-}
