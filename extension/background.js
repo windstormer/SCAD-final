3ff636f06b7770a116d2c97d5d1be48b71f8aeb2
windstormer <chenjuzen@gmail.com>
2016-12-26 22:02:07 +0800

    mix popup property, search feature

--- extension/background.js
+++ extension/background.js
@@ -6,6 +6,27 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript(null,{
   	file: "content_script.js"
     //code: 'document.body.style.backgroundColor="blue"'
-  });
+  	});
+  chrome.tabs.insertCSS(null, {
+  	file: "mystyle.css"
+  	});
+  chrome.tabs.executeScript({
+        file: 'js/bootstrap.js'
+  	});
+  chrome.tabs.executeScript({
+        file: 'js/bootstrap.min.js'
+  	});
+  // chrome.tabs.insertCSS(null, {
+  // 	file: "css/bootstrap.css"
+  // 	});
+  // chrome.tabs.insertCSS(null, {
+  // 	file: "css/bootstrap.min.css"
+  // 	});
+  chrome.tabs.insertCSS(null, {
+  	file: "css/bootstrap-theme.css"
+  	});
+  chrome.tabs.insertCSS(null, {
+  	file: "css/bootstrap-theme.min.css"
+  	});
 
 });
--- extension/content_script.js
+++ extension/content_script.js
@@ -1,46 +1,112 @@
-console.log("test!!!");
+//document.body.style.backgroundColor="red";
+console.log("test");
 
 
 
 var popup = document.createElement("div");
 popup.className = "image_box";
-popup.style.top = "10%";
-popup.style.left = "25%";
-popup.style.position = "fixed";
-popup.style.height = "50%";
-popup.style.width = "50%";
-popup.style.zIndex = "999";
-popup.style.backgroundColor = "blue";
-popup.style.visibility = "hidden";
+popup.style.visibility="hidden";
+
+// popup.className = "modal fade";
+// popup.id = "myModal";
+// popup.setAttribute("role","dialog");
+// var dialog = document.createElement("div");
+// dialog.className = "modal-dialog";
+// var content = document.createElement("div");
+// content.className = "modal-content";
+
+// var header_1 = document.createElement("div");
+// header_1.className = "modal-header";
+// var close = document.createElement("button")
+// close.className = "close";
+// close.setAttribute("data-dismiss","modal");
+// close.onclick = check;
+// close.innerHTML = '&times;';
+// var title = document.createElement("h4");
+// title.className = "modal-title";
+// title.innerHTML = "Modal Header";
+// header_1.appendChild(close);
+// header_1.appendChild(title);
+
+// var body = document.createElement("div");
+// body.className = "modal-body";
+// var text = document.createElement("p");
+// text.innerHTML = "Some text in the modal";
+// body.appendChild(text);
+
+// var footer = document.createElement("div");
+// footer.className = "modal-footer";
+
+// content.appendChild(header_1);
+// content.appendChild(body);
+// content.appendChild(footer);
+
+// dialog.appendChild(content);
+// popup.appendChild(dialog)
+
+ // popup.style.top = "10%";
+ // popup.style.left = "25%";
+ // popup.style.position = "fixed";
+ // popup.style.height = "50%";
+ // popup.style.width = "50%";
+ // popup.style.zIndex = "999";
+ // popup.style.backgroundColor = "blue";
+ // popup.style.visibility = "hidden";
+
+ //popup.innerHTML = '  <div class="modal fade" id="myModal" role="dialog"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal">&times;</button><h4 class="modal-title">Modal Header</h4></div><div class="modal-body"><p>Some text in the modal.</p></div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div></div>';
+// modal_temp = document.createElement("div");
+// modal_temp.className = "modal fade";
+// modal_temp.id = "myModal";
+// modal_temp.setAttribute("role","dialog");
+// modal_temp.innerHTML = '<div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal">&times;</button><h4 class="modal-title">Modal Header</h4></div><div class="modal-body"><p>Some text in the modal.</p></div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div>';
+
 var keyword = document.createElement("input");
 keyword.id="input";
+keyword.className = "form-control";
 var button = document.createElement("button");
+button.className = "btn btn-primary btn-sm";
 button.onclick = search;
 button.innerHTML = "Go!"
+var close = document.createElement("button")
+close.className = "close";
+close.setAttribute("data-dismiss","modal");
+close.onclick = check;
+close.innerHTML = '&times;';
+popup.appendChild(close);
 popup.appendChild(keyword);
 popup.appendChild(button);
-
+ console.log(popup);
 
 
 
 
 function check(event){
+    var ss = document.getElementsByClassName("_1osa mentionsHidden");
+      
+  for(var i = 0; i<ss.length;i++)
+    {
+      ss[i].focus();
+    }
   if(popup.style.visibility === 'hidden'){
     popup.style.visibility = 'visible';
+    document.getElementById("mainContainer").style.webkitFilter = "blur(10px)";
   } else {
     popup.style.visibility = 'hidden';
+    document.getElementById("mainContainer").style.webkitFilter = "blur(0px)";
   }
-  console.log(event.target.parentElement.parentElement);
-
+    console.log(event.target.parentElement.parentElement);
 }
 
 var list = document.getElementsByClassName("UFICommentAttachmentButtons");
+
 
 for(var i =0 ;i<list.length;i++)
 {
   var output = document.createElement("button");
   output.innerHTML = "test";
   output.className = "picture_talk";
+  // output.setAttribute("data-toggle","modal");
+  // output.setAttribute("data-target","#myModal");
   if(list[i].childNodes.length==2)  //prevent from append more than one time
     list[i].appendChild(output);
 }
